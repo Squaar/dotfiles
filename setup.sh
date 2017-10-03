f95ca974cfc9bf2ec60128debfeb120b012d4678
Matt Dumford <mdumford99@gmail.com>
2017-10-02 23:26:17 -0500

    fixed setup, vundle branch

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ ln -s ~/bin/dotfiles/.vimrc ~/.vimrc
 ln -s ~/bin/dotfiles/.vim ~/.vim
 ln -s ~/bin/dotfiles/.tmux.conf ~/.tmux.conf
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv ~/.zshrc ~/.zshrc.bak
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-
 git submodule init
 git submodule update
 vim +PluginInstall +qall
 
 git config --global user.name "Matt Dumford"
 git config --global user.email mdumford99@gmail.com
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv ~/.zshrc ~/.zshrc.bak
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
