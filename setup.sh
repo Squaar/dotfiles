220cd4b59ca8b5381e0fee5a6c381cd1b6f32447
Matt Dumford <mdumford99@gmail.com>
2017-11-04 16:34:34 -0500

    .gitconfig

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@ ln -s ~/bin/dotfiles/.zshrc ~/.zshrc
 ln -s ~/bin/dotfiles/.vimrc ~/.vimrc
 ln -s ~/bin/dotfiles/.vim ~/.vim
 ln -s ~/bin/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/bin/dotfiles/.gitconfig ~/.gitconfig
 
 git submodule init
 git submodule update
 vim +PluginInstall +qall
 vim +PluginUpdate
 
-git config --global user.name "Matt Dumford"
-git config --global user.email mdumford99@gmail.com
+# git config --global user.name "Matt Dumford"
+# git config --global user.email mdumford99@gmail.com
 
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # mv ~/.zshrc ~/.zshrc.bak
